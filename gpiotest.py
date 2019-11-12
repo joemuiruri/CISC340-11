2a52ecd3ec0bfddb0ef567ca13cb5c82860e2cae
Bennet Montgomery <bennetlogan@gmail.com>
2019-11-12 12:15:53 -0500

    added test script for rpi GPIO pins

---
+++ gpiotest.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import sys
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setwarnings(False)
+
+chan_list = [14, 15, 18]
+
+GPIO.setup(chan_list, GPIO.OUT)
+
+GPIO.output(chan_list, GPIO.LOW)
+
+for i in range(0, 3):
+	print("GPIO Output test: pin " + chan_list[i])
+	
+	GPIO.output(chan_list[i], GPIO.HIGH)
+	time.sleep(3)
+	GPIO.output(chan_list[i], GPIO.LOW)
+
+GPIO.cleanup()
