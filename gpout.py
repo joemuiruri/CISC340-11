279ec363b79cf7258dbf289ed522486500ff6fa2
joemuiruri <muiruri.vale@gmail.com>
2019-11-14 04:29:17 +0000

    refactored gpout.py after switch processing cut

--- gpout.py
+++ gpout.py
@@ -6,39 +6,14 @@ GPIO.setmode(GPIO.BCM)
 
 GPIO.setwarnings(False)
 
-chan_list = (14, 15, 18, 23)
+chan_list = [4, 17, 18, 23]
 
 GPIO.setup(chan_list, GPIO.OUT)
 
-for i in range(1, 6):
-	if(sys.argv[i] == '0'):
-		GPIO.output(chan_list, (False, False, False, False))
-		print("outputting 0")
-	elif(sys.argv[i] == '1'):
-		GPIO.output(chan_list, (True, False, False, False))
-		print("outputting 1")
-	elif(sys.argv[i] == '2'):
-		GPIO.output(chan_list, (False, True, False, False))
-		print("outputting 2")
-	elif(sys.argv[i] == '3'):
-		GPIO.output(chan_list, (True, True, False, False))
-		print("outputting 3")
-	elif(sys.argv[i] == '4'):
-		GPIO.output(chan_list, (False, False, True, False))
-		print("outputting 4")
-	elif(sys.argv[i] == '5'):
-		GPIO.output(chan_list, (True, False, True, False))
-		print("outputting 5")
-	elif(sys.argv[i] == '6'):
-		GPIO.output(chan_list, (False, True, True, False))
-		print("outputting 6")
-	elif(sys.argv[i] == '7'):
-		GPIO.output(chan_list, (True, True, True, False))
-		print("outputting 7")
+GPIO.output(chan_list, (True, False, False, False)) #setting alarm termination signal to basys3
 
-	time.sleep(2)
+time.sleep(2) #waiting 2 seconds guarantees basys3 receives termination signal
 
-
-GPIO.output(chan_list, (False, False, False, False))
+GPIO.output(chan_list, (False, False, False, False)) #resetting signal
 
 GPIO.cleanup()
