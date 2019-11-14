6c144f011cf2b7ae0654b76462ca94901efd7e6c
joemuiruri <muiruri.vale@gmail.com>
2019-11-14 04:48:08 +0000

    updated gpout to be used by serial_read when appropriate

--- gpout.py
+++ gpout.py
@@ -2,18 +2,14 @@ import RPi.GPIO as GPIO
 import sys
 import time
 
-GPIO.setmode(GPIO.BCM)
+def deactivateAlarm():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    chan_list = [4, 17, 18, 23] 
+    GPIO.setup(chan_list, GPIO.OUT)
+    GPIO.output(chan_list, (True, False, False, False)) #setting alarm termination signal to basys3
 
-GPIO.setwarnings(False)
+    time.sleep(2) #waiting 2 seconds guarantees basys3 receives termination signal
 
-chan_list = [4, 17, 18, 23]
-
-GPIO.setup(chan_list, GPIO.OUT)
-
-GPIO.output(chan_list, (True, False, False, False)) #setting alarm termination signal to basys3
-
-time.sleep(2) #waiting 2 seconds guarantees basys3 receives termination signal
-
-GPIO.output(chan_list, (False, False, False, False)) #resetting signal
-
-GPIO.cleanup()
+    GPIO.output(chan_list, (False, False, False, False)) #resetting signal
+    GPIO.cleanup()
